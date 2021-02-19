edff47f18a5b9ce4e01662f5ac68c4690bf158e3
Tim Etchells <tetchell@redhat.com>
2021-02-18 23:25:15 -0500

    Update build-images script for k8s-tools
    Signed-off-by: Tim Etchells <tetchell@redhat.com>

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eE -o pipefail
+set -eEu -o pipefail
 
 # Replace with your username. Don't push your dev images to redhat-github-actions.
 REGISTRY=${RUNNERS_REGISTRY:-quay.io/tetchell}
@@ -8,25 +8,45 @@ TAG=${RUNNERS_TAG:-latest}
 
 BASE_IMG=${REGISTRY}/runner:${TAG}
 BUILDAH_IMG=${REGISTRY}/buildah-runner:${TAG}
+K8S_TOOLS_IMG=${REGISTRY}/k8s-tools-runner:${TAG}
 
-set -x
+enabled() {
+    [[ $1 == *$2* ]]
+}
 
 cd $(dirname $0)
 
+echo "Building base image..."
 docker build ./base -f ./base/Dockerfile -t $BASE_IMG
-# docker build ./buildah -f ./buildah/Dockerfile -t $BUILDAH_IMG
 
-set +x
+if enabled "$*" buildah; then
+    echo "Building buildah image..."
+    docker build ./buildah -f ./buildah/Dockerfile -t $BUILDAH_IMG
+fi
+if enabled "$*" k8s; then
+    echo "Building K8s image..."
+    docker build ./k8s-tools -f ./k8s-tools/Dockerfile -t $K8S_TOOLS_IMG
+fi
 
-if [[ $1 == "push" ]]; then
-    set -x
+if enabled "$*" push; then
     docker push $BASE_IMG
-    # docker push $BUILDAH_IMG
+
+    if enabled "$*" buildah; then
+        docker push $BUILDAH_IMG
+    fi
+    if enabled "$*" k8s; then
+        docker push $K8S_TOOLS_IMG
+    fi
 else
-    echo "Not pushing. Set \$1 to 'push' to push"
+    echo "Not pushing. Pass 'push' to push"
 fi
 
 echo "$BASE_IMG"
-echo "$BUILDAH_IMG"
+if enabled "$*" buildah; then
+    echo "$BUILDAH_IMG"
+fi
+if enabled "$*" k8s; then
+    echo "$K8S_TOOLS_IMG"
+fi
 
 cd - > /dev/null
